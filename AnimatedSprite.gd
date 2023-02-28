9ba3d3b310b69f0bbf5f1bacd957edab5c8a8fd7
h_hue <28158318+hhues@users.noreply.github.com>
2023-02-28 09:11:53 -0500

    Fixed animated sprite rotation

---
+++ AnimatedSprite.gd
@@ -0,0 +1,9 @@
+extends AnimatedSprite
+
+export (NodePath) var TargetPath
+
+onready var TargetNode = get_node(TargetPath)
+onready var StartOffset = self.transform.origin - TargetNode.transform.origin
+
+func _process(delta):
+	self.transform.origin = TargetNode.transform.origin + StartOffset
--- Player/Player.gd
+++ Player/Player.gd
@@ -30,20 +30,20 @@ func get_input():
 	
 	if Input.is_action_pressed("player_right"):
 		dir_x += 1
-		$AnimatedSprite.play("right")
+		$"../AnimatedSprite".play("right")
 	if Input.is_action_pressed("player_left"):
 		dir_x -= 1
-		$AnimatedSprite.play("left")
+		$"../AnimatedSprite".play("left")
 	if dir_x != 0:
 		input.x = lerp(input.x, dir_x * speed, acceleration)
 	else:
 		input.x = lerp(input.x, 0, friction)
 	if Input.is_action_pressed("player_up"):
 		dir_y -= 1
-		$AnimatedSprite.play("up")
+		$"../AnimatedSprite".play("up")
 	if Input.is_action_pressed("player_down"):
 		dir_y += 1
-		$AnimatedSprite.play("down")
+		$"../AnimatedSprite".play("down")
 		
 	if dir_y != 0:
 		input.y = lerp(input.y, dir_y * speed, acceleration)
@@ -51,7 +51,7 @@ func get_input():
 		input.y = lerp(input.y, 0, friction)
 	
 	if dir_x == 0 and dir_y ==0:
-		$AnimatedSprite.play("down_stationary")
+		$"../AnimatedSprite".play("down_stationary")
 
 
 
